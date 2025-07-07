7715f724bfd12bc2641cd09dd729e9588b47a88d
eustephaniepereira <contato.stephaniep@icloud.com>
2025-07-07 20:02:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao= document.querySelector("#botao");
+const mensagem= document.querySelector("#mensagem");
+
+botao.addEventListener("click",function(){
+    mensagem.textContent="Você é mais capaz do que imagina — continue!!";
+
+});
